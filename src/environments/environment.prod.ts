5f46aca755d1584a90485621e615cf01dd62a0a0
EdgardoMD <60943753+EdgardoMD@users.noreply.github.com>
2021-04-02 19:13:33 -0300

    Cambios config prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA38kWS_hwehEkNUHjYYy8DSJH1RGrafzA",
+    authDomain: "angular-firebase-p1.firebaseapp.com",
+    projectId: "angular-firebase-p1",
+    storageBucket: "angular-firebase-p1.appspot.com",
+    messagingSenderId: "630799690046",
+    appId: "1:630799690046:web:1f4615faaa02da100ae46f"
+  }
 };
